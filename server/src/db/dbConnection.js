a7738c0f74774e360b4db3f0061fccd2b9e75efb
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-03 02:09:35 +0530

    db , (ip issues still pertaining

---
+++ server/src/db/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export async function dbConnection() {
+  try{
+    const conn = await mongoose.connect(`${process.env.MONGODB_URL}${process.env.MONGODB_DATABASE}`);
+    console.log(`database connected, ${conn.connection.host.config}`)
+  }catch(err){
+    return console.log("Didn't connect to database", err.message);
+  }
+}
