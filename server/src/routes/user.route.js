7dcad7973ffb51758c89e74cc0dcbaab9633e8d5
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-05 01:22:17 +0530

    userRouter

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,18 @@
 import express from "express";
 export const userRouter = express.Router();
-import { home, sampleUser } from "../controllers/user.controller.js";
+import { deleteAccount, getChannelProfile, login, logout, register, sampleUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/authentication.js";
 
-userRouter.get("/", home);
 userRouter.get("/sampleusers", sampleUser)
+userRouter.get("/channel/:channelId", getChannelProfile);
+
+userRouter.use(verifyJWT);
+
+userRouter.post("/login", login);
+userRouter.post("/logout", logout);
+userRouter.post("/register", register);
+userRouter.delete("/delete", deleteAccount);
+
+
+
+
